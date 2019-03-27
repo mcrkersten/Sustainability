afdb1bfe2e36735aa0e3b1edaedcb4ff105a2d1a
Max Kersten <max-kersten@live.nl>
2019-03-27 21:15:55 +0100

    [+] Project-Save

---
+++ Assets/Scrips/UI/TextFadeInOut.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextFadeInOut : MonoBehaviour {
+    // can ignore the update, it's just to make the coroutines get called for example
+    void Start() {
+        StartCoroutine(FadeTextToFullAlpha(1f, GetComponent<Text>()));
+    }
+
+
+
+    public IEnumerator FadeTextToFullAlpha(float t, Text i) {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
+        while (i.color.a < 1.0f) {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime / t));
+            yield return null;
+        }
+        StartCoroutine(FadeTextToZeroAlpha(1f, GetComponent<Text>()));
+    }
+
+    public IEnumerator FadeTextToZeroAlpha(float t, Text i) {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
+        while (i.color.a > 0.0f) {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
+            yield return null;
+        }
+        StartCoroutine(FadeTextToFullAlpha(1f, GetComponent<Text>()));
+    }
+}
